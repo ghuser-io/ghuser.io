8ff720c134e11495ff46f607b9d22ba434f907a9
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-06-24 23:31:26 +0300

    htmlStatic and domStatic.

--- reframe/pages/LandingPage.config.js
+++ reframe/pages/LandingPage.config.js
@@ -3,6 +3,8 @@ import Landing from '../views/Landing';
 const LandingPage = {
   route: '/',
   view: Landing,
+  htmlStatic: true,
+  domStatic: true
 };
 
 export default LandingPage;
--- reframe/pages/LogoPage.config.js
+++ reframe/pages/LogoPage.config.js
@@ -3,6 +3,8 @@ import BigLogo from '../views/BigLogo';
 const LogoPage = {
   route: '/logo',
   view: BigLogo,
+  htmlStatic: true,
+  domStatic: true
 };
 
 export default LogoPage;
