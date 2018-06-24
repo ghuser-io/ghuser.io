d60b345bc91e7e556fabcd62883c2cc2fa01468a
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-06-24 21:05:25 +0300

    Stateless react component.

--- reframe/views/BigLogo.js
+++ reframe/views/BigLogo.js
@@ -4,12 +4,10 @@ import Logo from './Logo';
 
 import './BigLogo.css';
 
-class BigLogo extends React.Component {
-  render() {
-    return (
-      <div className="big-logo"><Logo/></div>
-    );
-  }
-}
+const BigLogo = props => {
+  return (
+    <div className="big-logo"><Logo/></div>
+  );
+};
 
 export default BigLogo;
