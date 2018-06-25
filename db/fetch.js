20f315033cb35c3cd305531d2b0fe08d0bccf6af
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-06-25 15:01:15 +0300

    Use github-contribs.

--- db/fetch.js
+++ db/fetch.js
@@ -3,17 +3,42 @@
 
 (async () => {
 
-  const dbPath = './db.json';
-
   const fetch = require('fetch-retry');
   const fs = require('fs');
+  const githubContribs = require('@ghuser/github-contribs');
+  const ora = require('ora');
+
+  const dbPath = './db.json';
   const db = require(dbPath);
 
+  // See https://stackoverflow.com/a/28431880/1855917
+  const dateToString = date => {
+    return date.toISOString().substring(0, 10);
+  };
+  const today = dateToString(new Date());
+
   for (const user in db.users) {
+    console.log(`Fetching ${user}...`);
+
+    const githubSpinner = ora(`Fetching api.github.com/users/${user}...`).start();
     const ghData = await fetch(`https://api.github.com/users/${user}`);
     const ghDataJson = await ghData.json();
-    db.users[user] = ghDataJson;
+    db.users[user] = {...db.users[user], ...ghDataJson};
+    githubSpinner.succeed(`Fetched api.github.com/users/${user}`);
+
+    if (!db.users[user].contribs) {
+      db.users[user].contribs = {
+        fetched_at: '2000-01-01',
+        repos: []
+      };
+    }
+    const repos = await githubContribs(user, db.users[user].contribs.fetched_at, null, ora);
+    db.users[user].contribs.repos = [
+      ...new Set([...db.users[user].contribs.repos, ...repos])
+    ].sort();
+    db.users[user].contribs.fetched_at = today;
+
+    fs.writeFileSync(dbPath, JSON.stringify(db, null, 2) + '\n', 'utf-8');
   }
-  fs.writeFileSync(dbPath, JSON.stringify(db, null, 2) + '\n', 'utf-8');
 
 })();
