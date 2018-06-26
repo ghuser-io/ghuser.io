5b2a0e8cd64983c8e9fd1dc9e5716223aff5d232
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-06-26 10:22:55 +0300

    Added bio to profile.

---
+++ reframe/views/profile/leftpanel/Bio.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Bio.css';
+
+const Bio = props => (
+  <div className="bio">{props.text}</div>
+);
+
+export default Bio;
--- reframe/views/profile/leftpanel/LeftPanel.js
+++ reframe/views/profile/leftpanel/LeftPanel.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Avatar from './Avatar';
+import Bio from './Bio';
 import VCard from './VCard';
 
 const LeftPanel = props => (
-  <div className="col-3 p-0 pr-3">
+  <div className="col-3 p-0 pr-4">
     <Avatar url={props.user.avatar_url} />
     <VCard login={props.user.login} name={props.user.name} url={props.user.html_url} />
+    <Bio text={props.user.bio} />
   </div>
 );
 
