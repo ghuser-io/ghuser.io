6edbb6d5f3c1c57ef122cb44d832a2fc1a3e9207
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-06-28 00:29:36 +0300

    Fixed add-avatar icons.

--- reframe/views/profile/Avatar.js
+++ reframe/views/profile/Avatar.js
@@ -4,7 +4,9 @@ import './Avatar.css';
 
 const Avatar = props => {
   if (props.type == "add") { // special button for explaining how to add an avatar
-    return <div className={`avatar border border-white rounded ${props.classes}`}>+</div>;
+    return <div className={`avatar border border-white rounded ${props.classes}`}>
+             <span className="avatar-add-sign">+</span>
+           </div>;
   }
   return <img className={`avatar border border-white rounded ${props.classes}`} src={props.url} />;
 };
