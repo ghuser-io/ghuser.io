2aaf85b52fecba39be9a29a30a9e8996bae9e97c
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-08-07 16:50:59 +0300

    Now fetch public gists' amount.

--- db/fetchUserDetailsAndContribs.js
+++ db/fetchUserDetailsAndContribs.js
@@ -80,9 +80,9 @@ positional arguments:
       for (const field of ["id", "node_id", "gravatar_id", "followers_url", "following_url",
                            "gists_url", "starred_url", "subscriptions_url", "events_url",
                            "received_events_url", "site_admin", "hireable", "public_repos",
-                           "public_gists", "followers", "following", "private_gists",
-                           "total_private_repos","owned_private_repos", "disk_usage",
-                           "collaborators", "two_factor_authentication", "plan", "url"]) {
+                           "followers", "following", "private_gists", "total_private_repos",
+                           "owned_private_repos", "disk_usage", "collaborators",
+                           "two_factor_authentication", "plan", "url"]) {
         delete userFile[field];
       }
 
