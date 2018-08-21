276b7a4b592d42105307fa74c6b6ebb4b11dd1a6
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-08-21 09:06:56 +0300

    Simplified VCardDetails.js // #57

--- reframe/views/profile/leftpanel/VCardDetails.js
+++ reframe/views/profile/leftpanel/VCardDetails.js
@@ -6,71 +6,35 @@ import AddSettings from '../AddSettings';
 import './VCardDetails.css';
 
 const VCardDetails = props => {
-  const addSettings = !props.settings &&
-          <AddSettings href="https://github.com/AurelienLourot/ghuser.io/blob/master/docs/user-settings.md"
-                       title="Add a link" classes="ml-2" />;
+  const insertSettingsButtonOnce = (function () {
+    let button = !props.settings &&
+            <AddSettings
+              href="https://github.com/AurelienLourot/ghuser.io/blob/master/docs/user-settings.md"
+              title="Add a link" classes="ml-2" />;
+
+    return function () {
+      const result = button && React.cloneElement(button) || '';
+      button = null;
+      return result;
+    };
+  })();
 
   const details = [];
-  if (props.location) {
-    details.push(
-      <div className="vcard-detail pt-1" key="location">
-        <i className="vcard-icon fas fa-map-marker-alt"></i>&nbsp;
-        {props.location}
-        {!props.email && !props.blog && !props.gist_username && addSettings}
-      </div>
-    );
-  };
 
-  if (props.email) {
-    details.push(
-      <div className="vcard-detail pt-1" key="email">
-        <i className="vcard-icon far fa-envelope"></i> {Parser(Autolinker.link(props.email))}
-        {!props.blog && !props.gist_username && addSettings}
+  if (props.gist_username) {
+    details.unshift(
+      <div className="vcard-detail pt-1" key="gist">
+        <i className="vcard-icon fab fa-github"></i>&nbsp;
+        <a href={`https://gist.github.com/${props.gist_username}`} target="_blank"
+           className="external">GitHubGist</a>
+        {insertSettingsButtonOnce()}
       </div>
     );
-  };
-
-  if (props.blog) {
-    details.push(
-      <div className="vcard-detail pt-1" key="blog">
-        <i className="vcard-icon fas fa-link"></i> {Parser(Autolinker.link(props.blog, {
-          className: 'external'
-        }))}
-        {!props.gist_username && addSettings}
-      </div>
-    );
-  };
+  }
 
   if (props.settings) {
-    if (props.settings.twitter_username) {
-      details.push(
-        <div className="vcard-detail pt-1" key="twitter">
-          <i className="vcard-icon fab fa-twitter"></i>&nbsp;
-          <a href={`https://twitter.com/${props.settings.twitter_username}`} target="_blank"
-             className="external">@{props.settings.twitter_username}</a>
-        </div>
-      );
-    }
-    if (props.settings.reddit_username) {
-      details.push(
-        <div className="vcard-detail pt-1" key="reddit">
-          <i className="vcard-icon fab fa-reddit-alien"></i>&nbsp;
-          <a href={`https://reddit.com/user/${props.settings.reddit_username}`} target="_blank"
-             className="external">u/{props.settings.reddit_username}</a>
-        </div>
-      );
-    }
-    if (props.settings.linkedin_id) {
-      details.push(
-        <div className="vcard-detail pt-1" key="linkedin">
-          <i className="vcard-icon fab fa-linkedin"></i>&nbsp;
-          <a href={`https://linkedin.com/in/${props.settings.linkedin_id}`} target="_blank"
-             className="external">LinkedIn</a>
-        </div>
-      );
-    }
     if (props.settings.stackoverflow_id) {
-      details.push(
+      details.unshift(
         <div className="vcard-detail pt-1" key="stackoverflow">
           <i className="vcard-icon fab fa-stack-overflow"></i>&nbsp;
           <a href={`https://stackoverflow.com/users/${props.settings.stackoverflow_id}`}
@@ -78,18 +42,64 @@ const VCardDetails = props => {
         </div>
       );
     }
+    if (props.settings.linkedin_id) {
+      details.unshift(
+        <div className="vcard-detail pt-1" key="linkedin">
+          <i className="vcard-icon fab fa-linkedin"></i>&nbsp;
+          <a href={`https://linkedin.com/in/${props.settings.linkedin_id}`} target="_blank"
+             className="external">LinkedIn</a>
+        </div>
+      );
+    }
+    if (props.settings.reddit_username) {
+      details.unshift(
+        <div className="vcard-detail pt-1" key="reddit">
+          <i className="vcard-icon fab fa-reddit-alien"></i>&nbsp;
+          <a href={`https://reddit.com/user/${props.settings.reddit_username}`} target="_blank"
+             className="external">u/{props.settings.reddit_username}</a>
+        </div>
+      );
+    }
+    if (props.settings.twitter_username) {
+      details.unshift(
+        <div className="vcard-detail pt-1" key="twitter">
+          <i className="vcard-icon fab fa-twitter"></i>&nbsp;
+          <a href={`https://twitter.com/${props.settings.twitter_username}`} target="_blank"
+             className="external">@{props.settings.twitter_username}</a>
+        </div>
+      );
+    }
   };
 
-  if (props.gist_username) {
-    details.push(
-      <div className="vcard-detail pt-1" key="gist">
-        <i className="vcard-icon fab fa-github"></i>&nbsp;
-        <a href={`https://gist.github.com/${props.gist_username}`} target="_blank"
-           className="external">GitHubGist</a>
-        {addSettings}
+  if (props.blog) {
+    details.unshift(
+      <div className="vcard-detail pt-1" key="blog">
+        <i className="vcard-icon fas fa-link"></i> {Parser(Autolinker.link(props.blog, {
+          className: 'external'
+        }))}
+        {insertSettingsButtonOnce()}
       </div>
     );
-  }
+  };
+
+  if (props.email) {
+    details.unshift(
+      <div className="vcard-detail pt-1" key="email">
+        <i className="vcard-icon far fa-envelope"></i> {Parser(Autolinker.link(props.email))}
+        {insertSettingsButtonOnce()}
+      </div>
+    );
+  };
+
+  if (props.location) {
+    details.unshift(
+      <div className="vcard-detail pt-1" key="location">
+        <i className="vcard-icon fas fa-map-marker-alt"></i>&nbsp;
+        {props.location}
+        {insertSettingsButtonOnce()}
+      </div>
+    );
+  };
 
   return (
     <div className={details.length > 0 && 'border-top border-gray-light py-3' || ''}>{details}</div>
