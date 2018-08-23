acee4e781b81fbacef32080a5dded11007a907b2
Aurelien Lourot <aurelien.lourot@gmail.com>
2018-08-23 19:02:38 +0300

    Fixed hidden strerr when running fetchBot.

--- db/fetchBot/bot.sh
+++ db/fetchBot/bot.sh
@@ -2,6 +2,11 @@
 
 source raven-bash
 
+# By default raven-bash swallows stderr in order to send it to Sentry, see
+# https://github.com/ViktorStiskala/raven-bash#known-bugs . However since we own the machine where
+# this script is running, we prefer to have stderr on the console.
+exec 2>&1
+
 while true; do
   git pull
   pushd ../
