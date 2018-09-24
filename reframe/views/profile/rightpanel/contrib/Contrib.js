79b4bffb6cbbe1fe12839b5d5fb01ec5843c305f
Romuald Brillout <github.com@brillout.com>
2018-09-24 19:41:37 +0200

    improve ui - clean code

--- reframe/views/profile/rightpanel/contrib/Contrib.js
+++ reframe/views/profile/rightpanel/contrib/Contrib.js
@@ -1,26 +1,22 @@
 import React from 'react';
 
-// TODO never use wildcard imports
-import * as moment from 'moment';
 import * as db from '../../../../db';
 
 import './Contrib.css';
 
-/*
-import Badge from './Badge';
-*/
 import Avatar from '../../Avatar';
 import AvatarAdd from '../../AvatarAdd';
-import {bigNum, roundHalf, numberOf} from '../../numbers';
+import {numberOf} from '../../numbers';
 import {urls} from '../../../../ghuser';
 import {Badges, BadgesMini, BadgesMultiLine, getContribType} from './badges/Badges';
 import {getContribScore} from './getContribScore';
 import {getCommitCounts} from './getContribInfo';
 import RichText from './RichText';
-import {Accordion, AccordionHead, AccordionBody, AccordionIcon, AccordionBadgerIcon, stopPropagationOnLinks} from './Accordion';
+import {Accordion, AccordionHead, AccordionBody, AccordionBadgerIcon, stopPropagationOnLinks} from './Accordion';
 import Language from './Language';
 import AddSettings from '../../AddSettings';
 import ProgressBar from './ProgressBar';
+
 
 class Contrib extends React.Component {
   constructor(props) {
@@ -47,13 +43,13 @@ class Contrib extends React.Component {
         return <ContribMini {...{...this.props, ...this.state}}/>;
     }
 
-    const strStars = numStars => `★ ${bigNum(numStars)}`;
-    const strLastPushed = pushedAt => `last pushed ${moment(pushedAt).fromNow()}`;
-    const strNumCommits = numCommits => `${bigNum(numCommits)} non-merge commits`;
-
     const avatar = () => {
       if (this.state.loading) {
-        return <span className="contrib-name mb-2 mr-2"><i className="fas fa-spinner fa-pulse"></i></span>;
+        return (
+          <span className="mb-2 mr-2" style={{display: 'inline-block', verticalAlign: 'middle'}}>
+            <i className="fas fa-spinner fa-pulse"/>
+          </span>
+        );
       }
       if (this.state.repo && this.state.repo.settings && this.state.repo.settings.avatar_url) {
         return <Avatar url={this.state.repo.settings.avatar_url} classes="avatar-small" />;
@@ -72,73 +68,12 @@ class Contrib extends React.Component {
       );
     };
 
-    const badges = (owner, isFork, percentage, numContributors, popularity, numStars, activity,
-                    pushedAt, maturity, numCommits, isMaintainer) => {
-      const result = [];
-      if (!isFork && this.props.username === owner || percentage >= 80) {
-        result.push(
-            <Badge key="percentage" classes="badge-success contrib-name" text="owner"
-                   tooltip={`${this.props.username} wrote ${roundHalf(percentage)}% of it`}/>
-        );
-      } else if (isMaintainer) {
-        result.push(
-            <Badge key="percentage" classes="badge-danger contrib-name" text="maintainer"
-                   tooltip={`${this.props.username} wrote ${roundHalf(percentage)}% of it`}/>
-        );
-      }
-      if (numContributors > 1) {
-        result.push(<Badge key="collaborative" classes="badge-secondary contrib-name"
-                           text="collaborative"
-                           tooltip={`${numContributors} people worked on it`}/>);
-      }
-      if (popularity > 2.5) {
-        result.push(<Badge key="popular" classes="badge-secondary contrib-name" text="popular"
-                           tooltip={strStars(numStars)}/>);
-      }
-      if (activity > 2.5) {
-        result.push(<Badge key="active" classes="badge-secondary contrib-name" text="active"
-                           tooltip={strLastPushed(pushedAt)}/>);
-      }
-      if (maturity > 2.5) {
-        result.push(<Badge key="mature" classes="badge-secondary contrib-name" text="mature"
-                           tooltip={strNumCommits(numCommits)}/>);
-      }
-      return result;
-    };
-
-    const earnedStars = (percentage, numStars) => {
-      let earned = percentage * numStars / 100;
-      if (earned < .5) {
-        return '';
-      }
-
-      earned = bigNum(earned);
-      const total = bigNum(numStars);
-      let displayStr = `★ ${earned}`;
-      if (earned !== total) {
-        displayStr += ` / ${total}`;
-      }
-
-      return (
-        <span className="ml-2 mb-2 contrib-name text-gray">{displayStr}</span>
-      );
-    };
-
-    const userIsMaintainer = this.props.contrib.percentage >= 15;
-
     const LEFT_PADDING = 67;
 
     const badgesLine = (
       <Badges contrib={this.props.contrib} username={this.props.username} style={{marginTop: 3}}/>
     );
-    const accordionBody = (
-      <ContribExpandedContent
-        {...{...this.props, ...this.state}}
-        style={{paddingLeft: LEFT_PADDING}}
-      />
-    );
-
-    const head = (
+    const accordionHead = (
       <AccordionHead
         style={{paddingBottom: 15, paddingTop: 15, paddingLeft: LEFT_PADDING, position: 'relative'}}
         className="contrib-head"
@@ -152,13 +87,20 @@ class Contrib extends React.Component {
       </AccordionHead>
     );
 
+    const accordionBody = (
+      <ContribExpandedContent
+        {...{...this.props, ...this.state}}
+        style={{paddingLeft: LEFT_PADDING}}
+      />
+    );
+
     return (
         <Accordion
           pushToFunctionQueue={this.props.pushToFunctionQueue}
           className="border-bottom border-gray-light"
         >
-        {head}
-        {accordionBody}
+          {accordionHead}
+          {accordionBody}
         </Accordion>
     );
 
@@ -168,48 +110,7 @@ class Contrib extends React.Component {
           {avatar()}
         </div>
         <ContribHeader {...{...this.props, ...this.state}}/>
-        {/*
-        <h4 className="contrib-name mr-1">
-          <a href={`https://github.com/${this.props.contrib.full_name}`}
-             target="_blank" className="text-bold contrib-name external"
-             title={this.props.contrib.full_name}>{this.props.contrib.name}</a>
-          {
-            this.state.repo && this.state.repo.fork &&
-              <i className="fas fa-code-branch contrib-name ml-2 text-gray" title="fork"></i>
-          }
-        </h4>
-        */}
-        {
-          /*
-          this.state.repo &&
-            badges(this.state.repo.owner, this.state.repo.fork, this.props.contrib.percentage,
-                   Object.keys(this.state.repo.contributors || []).length, this.props.contrib.popularity,
-                   this.state.repo.stargazers_count, this.props.contrib.activity,
-                   this.state.repo.pushed_at, this.props.contrib.maturity,
-                   this.props.contrib.total_commits_count, userIsMaintainer)
-          */
-        }
-        {
-          /*
-          this.state.repo &&
-            earnedStars(this.props.contrib.percentage, this.state.repo.stargazers_count)
-          */
-        }
         {body}
-        {
-          /*
-          this.state.repo &&
-            <RepoDescrAndDetails contrib={this.props.contrib} descr={this.state.repo.description}
-              languages={this.state.repo.languages}
-              techs={this.state.repo.settings && this.state.repo.settings.techs || []}
-              pulls_authors={this.state.repo.pulls_authors}
-              strStars={strStars(this.state.repo.stargazers_count)}
-              strLastPushed={strLastPushed(this.state.repo.pushed_at)}
-              strNumCommits={strNumCommits(this.props.contrib.total_commits_count)}
-              username={this.props.username} userIsMaintainer={userIsMaintainer}
-              pushToFunctionQueue={this.props.pushToFunctionQueue}/>
-          */
-        }
       </div>
     );
   }
@@ -227,7 +128,7 @@ function ContribMini(props) {
       />
     );
 
-    const head = (
+    const accordionHead = (
         <AccordionHead
           style={{paddingTop: 3, paddingBottom: 3, position: 'relative', paddingLeft: LEFT_PADDING}}
         >
@@ -236,7 +137,7 @@ function ContribMini(props) {
         </AccordionHead>
     );
 
-    const body = (
+    const accordionBody = (
       <ContribExpandedContent
         {...props}
         style={{paddingTop: 10, paddingLeft: LEFT_PADDING}}
@@ -248,12 +149,11 @@ function ContribMini(props) {
         pushToFunctionQueue={props.pushToFunctionQueue}
         className="border-bottom border-gray-light"
       >
-        {head}
-        {body}
+        {accordionHead}
+        {accordionBody}
       </Accordion>
     );
 }
-
 
 function ContribHeader({username, contrib: {name, full_name}, repo}) {
       if( ! repo ) {
