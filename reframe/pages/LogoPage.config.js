0b18f88d8a189cfe2ce855f03649bd63294d10c6
Romuald Brillout <github.com@brillout.com>
2018-10-08 18:00:01 +0200

    [misc] typo

--- reframe/pages/LogoPage.config.js
+++ reframe/pages/LogoPage.config.js
@@ -3,7 +3,7 @@ import BigLogo from '../views/BigLogo';
 const LogoPage = {
   route: '/logo',
   view: BigLogo,
-  renderHtmlAtBuildTime: true
+  renderHtmlAtBuildTime: true,
   doNotRenderInBrowser: true
 };
 
