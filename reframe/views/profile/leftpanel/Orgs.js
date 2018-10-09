ba6d89644c45aa491bf45763362c2722237f856d
Romuald Brillout <github.com@brillout.com>
2018-10-09 22:51:21 +0200

    [minor] typo

--- reframe/views/profile/leftpanel/Orgs.js
+++ reframe/views/profile/leftpanel/Orgs.js
@@ -9,11 +9,11 @@ class Orgs extends React.Component {
   render() {
     const orgAvatar = org => (
       <a key={org.name} href={`https://github.com/${org.name}`} target="_blank" title={org.name}>
-        <Avatar url={org.avatarUrl} classes="avatar-org" />
+        <Avatar url={org.avatar_url} classes="avatar-org" />
       </a>
     );
 
-    const contributedTo = (this.props.orgsData||[]).filter(org => org.avatarUrl).map(org => orgAvatar(org));
+    const contributedTo = (this.props.orgsData||[]).filter(org => org.avatar_url).map(org => orgAvatar(org));
     if( contributedTo.length===0 ) {
       return null;
     }
