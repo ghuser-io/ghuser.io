9f3cd67d9d20f266ff59ac10e0127a12272fdb3d
Tim Lange <tim.lange@online.de>
2019-04-24 17:59:05 +0200

    FIX: Fixed navbar buttons

--- reframe/views/NavBar.js
+++ reframe/views/NavBar.js
@@ -8,11 +8,13 @@ class NavBar extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/"><Logo/></a>
-        <a role="button" className="btn btn-dark btn-sm ghuser-on-github" target="_blank"
-            href={urls.mainRepo}>
-          <i className="fab fa-github"></i> ghuser on GitHub
-        </a>
+        <div className="container-lg">
+          <a className="navbar-brand" href="/"><Logo/></a>
+          <a role="button" className="btn btn-dark btn-sm ghuser-on-github float-right" target="_blank"
+              href={urls.mainRepo}>
+            <i className="fab fa-github"></i> ghuser on GitHub
+          </a>
+        </div>
       </nav>
     );
   }
